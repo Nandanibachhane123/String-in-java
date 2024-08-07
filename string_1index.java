2f42c332232a900a7bfff3faad7478fe3dd3c2fa
Nandanibachhane123 <bachhanenandani@gmail.com>
2024-08-07 04:10:50 -0700

    Add files via upload

---
+++ string_1index.java
@@ -0,0 +1,15 @@
+//char at the index
+public class string_1index 
+{
+   public static void main(String[] args) 
+   {
+     String s = "Mahima";
+     char ch;
+     
+     for(int i = 0; i<s.length();i++)
+     {
+        ch = s.charAt(i);
+     }
+     System.out.println(s.charAt((3)));
+   } 
+}
