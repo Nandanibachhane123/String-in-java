8a294ad6c1b2b1e2717eba69f64e35365ab5fa58
Nandanibachhane123 <bachhanenandani@gmail.com>
2024-08-08 08:20:35 -0700

    Add files via upload

---
+++ String_6buffer.java
@@ -0,0 +1,9 @@
+public class String_6buffer 
+{
+   public static void main(String[] args)
+    {
+        StringBuffer s11 = new StringBuffer("example.com");
+        StringBuffer s22 = new StringBuffer("Example.com");
+        System.out.println(s11.compareTo(s22));
+   } 
+}
