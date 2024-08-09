438d61df233995ae27e7942e4162c70f2cbeb86b
Nandanibachhane123 <bachhanenandani@gmail.com>
2024-08-08 20:06:19 -0700

    Add files via upload

---
+++ String_7strobj.java
@@ -0,0 +1,8 @@
+public class String_7strobj
+ {
+    public static void main(String[] args) 
+    {
+        String s = new String("Stephen Edwin King");
+        String s2 = new String("Stephen Edwin King");
+    }
+}
